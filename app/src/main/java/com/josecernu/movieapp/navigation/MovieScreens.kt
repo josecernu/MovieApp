1a959b79a6de3678cbafcc178402b01afbb2eef3
José Manuel Cernuda <jlcu@gft.com>
2022-08-05 12:32:16 +0200

    Initial commit

---
+++ app/src/main/java/com/josecernu/movieapp/navigation/MovieScreens.kt
@@ -0,0 +1,16 @@
+package com.josecernu.movieapp.navigation
+
+//www.google.com/sign_in
+enum class MovieScreens {
+    HomeScreen,
+    DetailsScreen;
+    companion object {
+        fun fromRoute(route: String?): MovieScreens
+        = when (route?.substringBefore(delimiter = "/")) {
+            HomeScreen.name -> HomeScreen
+            DetailsScreen.name -> DetailsScreen
+            null -> HomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
+}
---
+++ app/src/main/java/com/josecernu/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,55 @@
+package com.josecernu.movieapp.screens.home
+
+import android.content.ContentValues
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.josecernu.movieapp.navigation.MovieScreens
+import com.josecernu.movieapp.widgets.MovieRow
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(
+            backgroundColor = Color.Transparent,
+            elevation = 0.dp) {
+            Text(text = "Movies")
+        }
+    },) {
+        MainContent(navController = navController)
+    }
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    movieList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "Harry Potter",
+    "Hapiness...",
+    "Cross the Line...",
+    "Be Happy...",
+    "Happy Feet...",
+    "Life")) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it) { movie ->
+                    navController.navigate(route = MovieScreens.DetailsScreen.name+"/$movie")
+                    //Log.d(ContentValues.TAG, "MainContent: $movie")
+                }
+            }
+        }
+    }
+}
